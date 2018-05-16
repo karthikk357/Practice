82a6b1b1d8c99565aaeb71fabb510740d70f9f58
karthikk357 <31151890+karthikk357@users.noreply.github.com>
2018-05-15 21:30:25 -0500

    ok

--- Example1/src/main/java/Sample.java
+++ Example1/src/main/java/Sample.java
@@ -1,8 +1,23 @@
-
+/**
+* test
+*/
 public class Sample {
+	///sample comment
 	public String hello() {
 		System.out.println("=================================");
-		return "Hello world";
+		System.out.println("=================================");
+		System.out.println("=================================");
+		System.out.println("=================================");
+		System.out.println("=================================");
+		return "Hello world @ today";
+	}
+	public void hello2() {
+		System.out.println("=================================");
+		System.out.println("=================================");
+		System.out.println("=================================");
+		System.out.println("=================================");
+		System.out.println("=================================");
+		
 	}
 
 }
